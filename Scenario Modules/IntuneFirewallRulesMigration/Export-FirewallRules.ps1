3ed17e81db2b2d0687f2ebdbe78a671a39dba7cd
joshbasquez <69749376+joshbasquez@users.noreply.github.com>
2024-03-14 12:25:31 -0500

    Update Export-FirewallRules.ps1

--- Scenario Modules/IntuneFirewallRulesMigration/Export-FirewallRules.ps1
+++ Scenario Modules/IntuneFirewallRulesMigration/Export-FirewallRules.ps1
@@ -24,18 +24,18 @@ if(-not(Test-Path ".\Intune-PowerShell-Management.zip")){
 }
 #----------------------------------------------------------------------------------------------
 
-## check for running from correct folder location
 
 
 
-
+#Import all the right modules
 Import-Module ".\Intune-PowerShell-Management-master\Scenario Modules\IntuneFirewallRulesMigration\FirewallRulesMigration.psm1"
 . ".\Intune-PowerShell-Management-master\Scenario Modules\IntuneFirewallRulesMigration\IntuneFirewallRulesMigration\Private\Strings.ps1"
 
+##Validate the user's profile name
 $profileName = ""
 try
 {
-    $json = Invoke-MSGraphRequest -Url "https://graph.microsoft.com/beta/deviceManagement/intents?$filter=templateId%20eq%20%274b219836-f2b1-46c6-954d-4cd2f4128676%27%20or%20templateId%20eq%20%274356d05c-a4ab-4a07-9ece-739f7c792910%27%20or%20templateId%20eq%20%275340aa10-47a8-4e67-893f-690984e4d5da%27" -HttpMethod GET
+    $json = Invoke-MSGraphRequest -Url "https://dod-graph.microsoft.us/beta/deviceManagement/intents?$filter=templateId%20eq%20%274b219836-f2b1-46c6-954d-4cd2f4128676%27%20or%20templateId%20eq%20%274356d05c-a4ab-4a07-9ece-739f7c792910%27%20or%20templateId%20eq%20%275340aa10-47a8-4e67-893f-690984e4d5da%27" -HttpMethod GET
     $profiles = $json.value
     $profileNameExist = $true
     $profileName = Read-Host -Prompt $Strings.EnterProfile
